4c1539e1782558721a8c7d93c2dcd8403099713f
toptops <seongtab@naver.com>
2020-08-13 21:20:43 +0900

    [2020-08-13 배성탑] QueryDsl 강의 -8 - 조인 on 절

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -256,4 +256,48 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL : select m, t from Member m left join m.team t on t.name = 'teamA'
+     *
+     * left 조인을 할 경우 on절을 이용하고 일반 조인을 사용할 경우에는 where절을 사용하자.
+     */
+    @Test
+    public void join_on_filtering() {
+       List<Tuple> result = queryFactory
+                    .select(member, team)
+                    .from(member)
+                    .join(member.team, team)
+                    .where(team.name.eq("teamA"))
+//                    .leftJoin(member.team, team)
+//                    .on(team.name.eq("teamA"))
+                    .fetch();
+
+       for(Tuple tuple : result) {
+           System.out.println("tuple : " + tuple);
+       }
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+//                .leftJoin(team).on(member.username.eq(team.name))
+                .join(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for(Tuple tuple : result) {
+            System.out.println("tuple : " + tuple);
+        }
+    }
 }
